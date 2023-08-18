16de5aa7e3e2c3267b11e069fd9ebc91071dfba8
NikeshDC <nikeshdc111@gmail.com>
2023-08-18 16:29:31 +0545

    added ship trails and enhanced scene

--- Assets/Scripts/CameraFollowShip.cs
+++ Assets/Scripts/CameraFollowShip.cs
@@ -7,9 +7,9 @@ public class CameraFollowShip : MonoBehaviour
 {
     private Transform target;
     [SerializeField] private Vector3 offset;
-    [SerializeField] private Vector3[] movementOffset = new Vector3[ButtonController.totalBoatCount];
+    [SerializeField] private Vector3[] movementOffset = new Vector3[GameController.totalBoatCount];
     [SerializeField] private float damping = 0.5f;
-    [SerializeField] private ButtonController buttonControllerScript;
+    [SerializeField] private GameController GameControllerScript;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Vector3 ShipSelectionRotation = new Vector3(0, -135, 0);
@@ -20,44 +20,38 @@ public class CameraFollowShip : MonoBehaviour
     {
         public float offsetValueX;
     }
-    public StatinoaryOffsetCameraX[] offsetValuesX = new StatinoaryOffsetCameraX[ButtonController.totalBoatCount];
+    public StatinoaryOffsetCameraX[] offsetValuesX = new StatinoaryOffsetCameraX[GameController.totalBoatCount];
 
     [Serializable]
     public struct StatinoaryOffsetCameraY
     {
         public float offsetValueY;
     }
-    public StatinoaryOffsetCameraY[] offsetValuesY = new StatinoaryOffsetCameraY[ButtonController.totalBoatCount];
+    public StatinoaryOffsetCameraY[] offsetValuesY = new StatinoaryOffsetCameraY[GameController.totalBoatCount];
 
     [Serializable]
     public struct StatinoaryOffsetCameraZ
     {
         public float offsetValueZ;
     }
-    public StatinoaryOffsetCameraZ[] offsetValuesZ = new StatinoaryOffsetCameraZ[ButtonController.totalBoatCount];
+    public StatinoaryOffsetCameraZ[] offsetValuesZ = new StatinoaryOffsetCameraZ[GameController.totalBoatCount];
 
-    private void Start()
-    {
-        if (!buttonControllerScript.playIsClicked)
-        {
-            transform.eulerAngles = ShipSelectionRotation;
-        }
-    }
     private void FixedUpdate()
     {
-        if (!buttonControllerScript.playIsClicked)
+        if (!GameControllerScript.playIsClicked)
         {
-            for (int i = 0; i < ButtonController.totalBoatCount; i++)
+            transform.eulerAngles = ShipSelectionRotation;
+            for (int i = 0; i < GameController.totalBoatCount; i++)
             {
-                if (buttonControllerScript.Ships[i].gameObject.activeSelf)
+                if (GameControllerScript.Ships[i].gameObject.activeSelf)
                 {
                     transform.position = new Vector3(offsetValuesX[i].offsetValueX, offsetValuesY[i].offsetValueY, offsetValuesZ[i].offsetValueZ);
                 }
             }
         }
-        if (buttonControllerScript.playIsClicked)
+        if (GameControllerScript.playIsClicked)
         {
-            target = buttonControllerScript.selectedShipToPlay;
+            target = GameControllerScript.selectedShipToPlay;
             transform.eulerAngles = ShipPlayRotation;
             Vector3 movePosition = target.position + offset;
             transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
@@ -66,86 +60,38 @@ public class CameraFollowShip : MonoBehaviour
             if (target.name == "ShipA")
             {
                 offset = movementOffset[0];//-43,90,-106
+                damping = 0.5f;
             }
             if (target.name == "ShipB")
             {
                 offset = movementOffset[1];//-43,90,-106
+                damping = 0.5f;
             }
             if (target.name == "ShipC")
             {
                 offset = movementOffset[2];//-43,90,-106
+                damping = 0.4f;
             }
             else if (target.name == "ShipD")
             {
                 offset = movementOffset[3];//-43,71,-76
+                damping = 0.3f;
             }
             else if (target.name == "ShipE")
             {
                 offset = movementOffset[4];//-18,40,-56
+                damping = 0.1f;
             }
             else if (target.name == "ShipF")
             {
                 offset = movementOffset[5];//-43,71,-76
+                damping = 0.3f;
             }
             else if (target.name == "ShipG")
             {
                 offset = movementOffset[6];//-22,43,-44
+                damping = 0.5f;
             }
         }
     }
 }
-/*
- 	    {0,cameraOffsetX[0] },//Scale 1,1,1 : 25
-        {1,cameraOffsetX[1] },//Scale 1,1,1 : 30
-        {2,cameraOffsetX[2] },//Scale 1,1,1 : 27
-        {3,cameraOffsetX[3] },//Scale 1,1,1 : 18
-        {4,cameraOffsetX[4] },//Scale 1,1,1 : 8
-        {5,cameraOffsetX[5] },//Scale 1,1,1 : 21
-        {6,cameraOffsetX[6] }//Scale 1,1,1 : 10
-
-        {0,cameraOffsetY[0] },//Scale 1,1,1 : 15
-        {1,cameraOffsetY[1] },//Scale 1,1,1 : 15
-        {2,cameraOffsetY[2] },//Scale 1,1,1 : 18
-        {3,cameraOffsetY[3] },//Scale 1,1,1 : 10
-        {4,cameraOffsetY[4] },//Scale 1,1,1 : 4
-        {5,cameraOffsetY[5] },//Scale 1,1,1 : 12
-        {6,cameraOffsetY[6] }//Scale 1,1,1 : 5
-
-        {0,cameraOffsetZ[0] },//Scale 1,1,1 : 40
-        {1,cameraOffsetZ[1] },//Scale 1,1,1 : 40
-        {2,cameraOffsetZ[2] },//Scale 1,1,1 : 38
-        {3,cameraOffsetZ[3] },//Scale 1,1,1 : 27
-        {4,cameraOffsetZ[4] },//Scale 1,1,1 : 13
-        {5,cameraOffsetZ[5] },//Scale 1,1,1 : 36
-        {6,cameraOffsetZ[6] }//Scale 1,1,1 : 14
-
-//scale 0.1
-Offset Values X
-2.5
-2.85
-2.95
-1.63
-0.88
-1.94
-1
-
-Offset Values Y
-1.33
-1.75
-1.72
-0.91
-0.4
-1.05
-0.47
-
-Offset Values Z
-4
-4.25
-4.28
-2.95
-1.3
-3.69
-1.56
-//scale 0.1
-
- */ 
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static int totalBoatCount = 7;
+    public GameObject actualShipsPrefab;
+    public Transform selectedShipToPlay;
+    public Transform[] Ships = new Transform[totalBoatCount];//an array of 7 ships
+
+    [SerializeField] private GameObject leftButton;
+    [SerializeField] private GameObject rightButton;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject resetButton;
+    [SerializeField] private GameObject joystick;
+    public bool playIsClicked;
+    private GameObject myShipPrefab;
+
+    private void Awake()
+    {
+        myShipPrefab = Instantiate(actualShipsPrefab);
+        for (int i = 0; i < totalBoatCount; i++)
+        {
+            Ships[i] = myShipPrefab.transform.GetChild(i);
+            Ships[i].GetComponent<BoatMovement>().gameController = this;
+            Ships[i].GetComponent<BoatMovement>().joystick = joystick.GetComponent<FixedJoystick>();
+        }
+    }
+
+    private void Start()
+    {
+        joystick.SetActive(false);
+        resetButton.SetActive(false);
+        playIsClicked = false;
+        Ships[0].gameObject.SetActive(true);
+        Ships[0].GetChild(3).gameObject.SetActive(false);
+        for (int i = 1; i < totalBoatCount; i++)
+        {
+            Ships[i].gameObject.SetActive(false);
+            Ships[i].GetChild(3).gameObject.SetActive(false);//disable particle effect for all ships
+        }
+    }
+    public void OnClickLeft()
+    {
+        for (int i = 1; i < totalBoatCount; i++)//omit the first boat
+        {
+            if (Ships[i].gameObject.activeInHierarchy == true)
+            {
+                Ships[i].gameObject.SetActive(false);
+                Ships[i - 1].gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+    public void OnClickRight()
+    {
+        for (int i = 0; i < totalBoatCount - 1; i++)//omit the last boat
+        {
+            if (Ships[i].gameObject.activeInHierarchy == true)
+            {
+                Ships[i].gameObject.SetActive(false);
+                Ships[i + 1].gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    public void OnClickPlay()
+    {
+        leftButton.SetActive(false);
+        rightButton.SetActive(false);
+        playButton.SetActive(false);
+        joystick.SetActive(true);
+        resetButton.SetActive(true);
+        selectedShipToPlay = ActiveShip();
+        selectedShipToPlay.GetChild(3).gameObject.SetActive(true);
+        playIsClicked = true;
+    }
+
+    public void OnClickReset()
+    {
+        Destroy(myShipPrefab);
+        Awake();
+        leftButton.SetActive(true);
+        rightButton.SetActive(true);
+        playButton.SetActive(true);
+        Start();
+    }
+
+    private Transform ActiveShip()
+    {
+        //Return the one ship that is currently active
+        for (int i = 0; i < totalBoatCount; i++)
+        {
+            if (Ships[i].gameObject.activeInHierarchy)
+            {
+                return Ships[i].transform;
+            }
+        }
+        return null;
+    }
+}
