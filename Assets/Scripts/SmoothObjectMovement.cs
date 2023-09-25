1c63c3a13dcd7f48a82c7de2aab52803b1774da6
NikeshDC <nikeshdc111@gmail.com>
2023-09-25 15:18:32 +0545

    added support for callback when finished executing

--- Assets/Scripts/SmoothObjectMovement.cs
+++ Assets/Scripts/SmoothObjectMovement.cs
@@ -9,6 +9,8 @@ public class SmoothObjectMovement : MonoBehaviour
     public float rotationSpeed = 1f;
     private Vector3 velocity;
 
+    public delegate void OnFinishCallback();
+
     private static void AddThisComponentTo(GameObject gameObject)
     {
         SmoothObjectMovement mover = gameObject.GetComponent<SmoothObjectMovement>();
@@ -16,44 +18,48 @@ public class SmoothObjectMovement : MonoBehaviour
             gameObject.AddComponent<SmoothObjectMovement>();
     }
 
-    public static void MoveObjectTo(GameObject gameObject, Vector3 newpos)
+    public static void MoveObjectTo(GameObject gameObject, Vector3 newpos, OnFinishCallback callbackFunction = null)
     {
         AddThisComponentTo(gameObject);
         SmoothObjectMovement mover = gameObject.GetComponent<SmoothObjectMovement>();
-        mover.MoveTo(newpos);
+        mover.MoveTo(newpos, callbackFunction);
     }
-    public static void RotateObjectTo(GameObject gameObject, Quaternion newRot)
+    public static void RotateObjectTo(GameObject gameObject, Quaternion newRot, OnFinishCallback callbackFunction = null)
     {
         AddThisComponentTo(gameObject);
         SmoothObjectMovement mover = gameObject.GetComponent<SmoothObjectMovement>();
-        mover.RotateTo(newRot);
+        mover.RotateTo(newRot, callbackFunction);
     }
 
-    public void MoveTo(Vector3 newpos)
+    public void MoveTo(Vector3 newpos, OnFinishCallback callbackFunction = null)
     {
         StopCoroutine("moveTo");
-        StartCoroutine("moveTo", newpos);
+        StartCoroutine(moveTo(newpos, callbackFunction));
     }
-    IEnumerator moveTo(Vector3 newpos)
+    IEnumerator moveTo(Vector3 newpos, OnFinishCallback callbackFunction = null)
     {
         while((transform.position - newpos).magnitude > moveThreshold)
         {
             transform.position = Vector3.SmoothDamp(transform.position, newpos, ref velocity, damping);
             yield return null;
         }
+        if(callbackFunction != null)
+            callbackFunction();
     }
 
-    public void RotateTo(Quaternion newRot)
+    public void RotateTo(Quaternion newRot, OnFinishCallback callbackFunction = null)
     {
         StopCoroutine("rotateTo");
-        StartCoroutine("rotateTo", newRot);
+        StartCoroutine(rotateTo(newRot, callbackFunction));
     }
-    IEnumerator rotateTo(Quaternion newRot)
+    IEnumerator rotateTo(Quaternion newRot, OnFinishCallback callbackFunction = null)
     {
         while (Quaternion.Angle(transform.rotation, newRot) > 0.5f)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, newRot, rotationSpeed * Time.deltaTime);
             yield return null;
         }
+        if (callbackFunction != null)
+            callbackFunction();
     }
 }
